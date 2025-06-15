08b67b04dcb530a887f4651f9419fdb92579f341
Tom Lynch <twhlynch@gmail.com>
2025-06-15 21:51:20 +1000

    improved build setup

--- build.sh
+++ build.sh
@@ -1,4 +1,29 @@
-mkdir -p build
-cd build
-cmake ..
-make
+#!/bin/bash
+
+# ./build.sh [Generator] [Config]
+
+generator=""
+config="Debug"
+
+if [[ "$1" == "Release" ]]; then
+	config="Release"
+elif [[ -n "$1" ]]; then
+	generator="$1"
+	if [[ "$2" == "Release" ]]; then
+		config="Release"
+	fi
+fi
+
+folder_name="${generator// /_}"
+[ -z "$folder_name" ] && folder_name="default"
+
+mkdir -p "build/$folder_name"
+cd "build/$folder_name"
+
+if [[ -n "$generator" ]]; then
+	cmake -G "$generator" ../..
+else
+	cmake ../..
+fi
+
+cmake --build . --config "$config"
